aa062d6d055692aa7ca21806f50b1be52ffc0b60
satvik1257 <83113815+satvik1257@users.noreply.github.com>
2021-07-14 12:55:34 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,9 +14,9 @@ document.getElementById("user_name").innerHTML="Welcome "+user_name+"!";
  {
      room_name= document.getElementById("room_name").value;
       localStorage.setItem("room_name",room_name);
-      window.location="kwitter_page.html";
+      window.location="kwitter_room.html";
  }
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       });});}
-getData();
+
